4a8cc262502dc5089baa41269cf9e77399be0c44
karthik1896 <karthik1896@outlook.com>
2023-04-16 15:04:45 +0530

    X00TD: Adapt to 404
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

---
+++ p404_X00TD.mk
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Project 404 Stuff
+$(call inherit-product, vendor/404/configs/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+P404_BUILDTYPE=TOKUI
+
+# Include GAPPS
+# WITH_GAPPS := true
+
+# Inherit from X00TD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := X00TD
+PRODUCT_MANUFACTURER := asus
+PRODUCT_NAME := p404_X00TD
+PRODUCT_MODEL := Zenfone Max Pro M1
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X00TD
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=X00TD \
+    PRODUCT_DEVICE=X00TD \
+    PRODUCT_NAME=WW_X00TD \
+    PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 1309 release-keys"
